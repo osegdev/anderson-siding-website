21855f328508b0fb9eb4333e85a62dce1b5215ed
osegdev <omarosegdev@gmail.com>
2025-05-24 19:54:55 -0600

    chore: setup basic project folders

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-red-800 text-white p-4">
+      <h1 className="text-xl font-bold">Anderson Siding and Construction</h1>
+    </header>
+  );
+}
